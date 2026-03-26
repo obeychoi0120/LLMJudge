16f631fd0a1fddbf46d0e13ec587f61fadb20f41
obeychoi0120 <wnstnc0120@gmail.com>
2026-03-26 02:59:52 +0000

    added repo update bash

---
+++ update_repo.sh
@@ -0,0 +1,6 @@
+git remote remove origin
+git remote add origin https://github.com/obeychoi0120/LLMJudge
+git pull origin main
+git remote remove origin
+git remote add origin https://github.sec.samsung.net/junsu2-choi/LLMJudge
+git push --set-upstream origin main
